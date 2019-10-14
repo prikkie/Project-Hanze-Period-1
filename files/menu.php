189ac8353807bd63dee37f42f7a6e4e23a0bc368
prikkie <prikantuma@hotmail.com>
2019-10-14 14:46:45 +0200

    Updated menu with dropdown

--- files/menu.php
+++ files/menu.php
@@ -1,31 +1,18 @@
-<script type="text/javascript">
-    function handleSelect(elm) {
-        window.location = elm.value;
-    }
-</script>
+<link href="/css/menu.css" rel="stylesheet" type="text/css"/>
 
-<ul class="menu">
-    <li><a href="/home" target="_self"><span>Home</span></a></li>
-    <li><a href="/product" target="_self"><span>Producten</span></a></li>
-    <li><a href="/winkelmandje" target="_self"><span>Winkelmandje</span></a></li>
+<div class="topnav" id="myTopnav">
+    <a href="/home" class="active">Home</a>
+    <a href="/products">Producten</a>
+    <a href="/home2">Home 2</a>
+    <div class="dropdown">
+        <button class="dropbtn">Dropdown
+            <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+            <a href="/login">Login</a>
+            <a href="/logout">Logout</a>
 
-    <?php
-    if ($_SESSION['logged_in'] == true) {
-        //laat persoonlijke pagina's zien
-        echo ">Logout</option>
-</select>
-
-";
+        </div>
+    </div>
 
 
-    } else {
-        //laat login knop zien
-        echo ">Logout</option>
-</select>
-
-
-
-\"";
-    }
-    ?>
-</ul>
