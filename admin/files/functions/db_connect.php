d87c830bd3aca195b9d2013b00f4c10a20ebbb4a
prikkie <prikantuma@hotmail.com>
2019-10-14 15:48:54 +0200

    Admin pages updated with CSS

--- admin/files/functions/db_connect.php
+++ admin/files/functions/db_connect.php
@@ -1,16 +1,15 @@
 <?php
 $dbservername = "localhost";
-$dbusername = "id141058_project";
-$dbpassword = "1rick5";
-$database = "id141058_project";
+$dbusername = "u636479626_pro";
+$dbpassword = "F08@q>91?";
+$database = "u636479626_project";
 
 // Create connection
 $conn = new mysqli($dbservername, $dbusername, $dbpassword, $database);
 
 // Check connection
-   if(! $conn )
-   {
-     die('Could not connect: ' . mysqli_error($conn));
-   }
+if (!$conn) {
+    die('Could not connect: ' . mysqli_error($conn));
+}
 
 ?>
--- admin/index.php
+++ admin/index.php
@@ -1,12 +1,13 @@
 <?php
-include 'functions/functions.php';
-require 'functions/db_connect.php';
+include 'files/functions/functions.php';
+require 'files/functions/db_connect.php';
 
 //Kijkt welke pagina opgevraagd is, kijkt of het bestaat, veranderd $pagina. Bestaat deze niet? Gaat de gebruiker naar de home pagina
 if (isset($_GET['nav']) && file_exists('files/' . $_GET['nav'] . '.php')) {
     $pagina = $_GET['nav'];
 } else {
-    header("Location: https://admin.projecthanze.com/home");
+    echo "hey!";
+    header("Location: http://admin.projecthanze.com/home");
 }
 
 ?>
@@ -16,7 +17,7 @@ if (isset($_GET['nav']) && file_exists('files/' . $_GET['nav'] . '.php')) {
         <?php
         // Als $pagina niet geinitialiseerd is, gaat hij tergu naar home. Anders geeft hij de $pagina weer.
         if (empty($pagina)) {
-            header("Location: https://admin.projecthanze.com/home");
+            header("Location: /home");
         } else {
             include("files/" . $pagina . ".php");
         }
