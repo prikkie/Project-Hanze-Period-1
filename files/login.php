19affc5e6c7c27f1971df0ec3654a82caf54973c
prikkie <prikantuma@hotmail.com>
2019-10-14 14:13:37 +0200

    wachtwoord

--- files/login.php
+++ files/login.php
@@ -1,9 +1,15 @@
 <?php
-if (isset($_POST['ww'])) {
+//kijkt of er iets is ingevuld en gaat dan ingevulde met database vergelijken. Klopt dit? dan $_SESSION['logged_in'] = true
+if (isset($_POST['ww']) && isset($_POST['gebruikersnaam'])) {
     $gebruikersnaam = $_POST['gebruikersnaam'];
     $wachtwoord = $_POST['ww'];
     $query = "select gebruikersnaam, wachtwoord from users where gebruikersnaam = '$gebruikersnaam' AND wachtwoord = $wachtwoord";
     mysqli_query($db, $query);
     $_SESSION['logged_in'] = true;
 }
+
+//test
+$_SESSION['logged_in'] = true;
 echo "hey";
+
+// hieronder tabel voor inloggen
