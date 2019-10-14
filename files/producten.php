5f253c1ea73a0a06b959f5c23274c5321caf0242
prikkie <prikantuma@hotmail.com>
2019-10-14 15:52:36 +0200

    products added

---
+++ files/producten.php
@@ -0,0 +1,30 @@
+<?php
+$query = "SELECT * FROM products";
+
+
+if ($result = $conn->query($query)) {
+    while ($row = $result->fetch_assoc()) {
+        $p_naam = $row["naam"];
+        $p_prijs = $row["prijs"];
+        $p_voorraad = $row["voorraad"];
+        $p_afbeelding = $row["afbeelding"];
+        $p_omschrijving = $row["omschrijving"];
+        $p_categorie = $row["categorie"];
+        $p_actief = $row["actief"];
+
+        echo "<div id = 'wrappert'>";
+        if ($p_actief == 0) {
+            echo "<div class = 'product'>" .
+                "Hoi de naam van het product is " . $p_naam . " !" . "</br>" .
+                "<img src = images/$p_afbeelding.jpg>" . "</br>"
+                . "Dit product kost: &euro;" . $p_prijs . "</br>"
+                . "Categorie: $p_categorie" . "</br>" . "</br>"
+                . "Omschrijving: $p_omschrijving" . "</br>"
+                . "<button type='button'>Voeg toe</button> "
+                . "</div>";
+        }
+        echo "</div>";
+    }
+    $result->free();
+}
+?>
